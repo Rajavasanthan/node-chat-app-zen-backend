1aed17a068cf8e7863e2dfef741b96711a8fe29e
rajavasanthan <Rajavasanthan@gmail.com>
2020-06-20 11:07:32 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const socketIO = require("socket.io");
+const app = express();
+
+
+const http = require("http");
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.get("/",function(req,res){
+     res.status(200).json({
+          message : "Success"
+     })
+});
+
+
+io.on("connection",function(socket){
+     console.log("New User Connected", socket.id);
+
+     socket.on("message",(data) => {
+          console.log(data)
+          socket.broadcast.emit('new-message', data);
+     })
+
+     socket.on("disconnect",function(){
+          console.log(socket.id," Disconnected")
+     });
+});
+
+
+
+server.listen(3000,function(){
+     console.log("App running in port 3000");
+})
